de5bf664bbaf00ad2965ffca3d0632e4adc56414
MDORRON <MIKELDORRONSOROMENDIGUREN@GMAIL.COM>
2025-11-17 13:56:42 +0100

    Primer commit: calculadora simple

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+# calculadora.py
+
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    x = int(input("Introduce el primer número: "))
+    y = int(input("Introduce el segundo número: "))
+    print("La suma es:", sumar(x, y))
